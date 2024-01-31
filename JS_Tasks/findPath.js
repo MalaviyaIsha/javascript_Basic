11058840b9e86d0fa941fdcabf8cc22a7cf51459
MalaviyaIsha <isha.malaviya@exhibytesolution.com>
2024-01-31 18:13:19 +0530

    Task : find the route from start position to exit in the matrix also You begin from the left side of the square in the top-left corner the exit is on the right bottom corner ,you must travel along a connected path netween squares are updated and completed.

--- JS_Tasks/findPath.js
+++ JS_Tasks/findPath.js
@@ -1,4 +1,9 @@
 // const matrix = [
+//   [0, 0, 2],
+//   [5, 4, 4],
+// ];
+
+// const matrix = [
 //   [0, 2, 1],
 //   [5, 4, 0],
 // ];
@@ -25,60 +30,58 @@ let directions = {
   5: [1, 2]
 }
 
-// const lastRow = matrix.length - 1;
-// console.log("lastRow :", lastRow)
-// const lastCol = matrix[lastRow].length - 1;
-// console.log("lastCol :", lastCol)
-// const lastElement = matrix[lastRow][lastCol];
-// console.log("lastElement :", lastElement)
-
 //function to get the next block for path::::::
 function getnextPosition(i, j, lastPosition) {
-//  debugger;
-  const matrixValue = matrix[i][j];
-  console.log(i, j);
-  console.log("matrixValue :::", matrixValue);
+  // debugger;
+  const start = matrix[0][0];
+  const end = matrix[matrix.length - 1][matrix[0].length - 1];
 
-  const edge = directions[matrixValue];
-  console.log("edge :::", edge);
-
-  console.log("connection[lastPosition] :::", connection[lastPosition]);
-
-  const startIndex = edge.indexOf(connection[lastPosition]);
-  console.log("startIndex :::", startIndex);
-
-  if (startIndex < 0) {
+  if (!(start === 0 || start === 2) || !(end === 0 || end === 4)) {
     return false;
   }
+  else {
+    const matrixValue = matrix[i][j];
+    // console.log(i, j);
+    // console.log("matrixValue :::", matrixValue);
 
-  //get the endEdge value:::::::::
-  const endEdge = edge[startIndex == 0 ? 1 : 0];
-  console.log("endEdge :::", endEdge);
+    const edge = directions[matrixValue];
+    // console.log("edge :::", edge);
+    // console.log("connection[lastPosition] :::", connection[lastPosition]);
 
-  //check the end cell of the matrix::::::
-  if (i === matrix.length - 1 || j === matrix[0].length - 1) {
-    if (endEdge == 3) {
-      return true;
-    }
-    else {
+    const startIndex = edge.indexOf(connection[lastPosition]);//1
+    // console.log("startIndex :::", startIndex);
+
+    if (startIndex < 0) {
       return false;
     }
-  }
 
+    //get the endEdge value:::::::::
+    const endEdge = edge[startIndex == 0 ? 1 : 0];
+    // console.log("endEdge :::", endEdge);
 
-  if (endEdge == 1) {
-    return getnextPosition(i, j - 1, 1)
-  }
-  if (endEdge == 2) {
-    return getnextPosition(i - 1, j, 2)
-  }
-  if (endEdge == 3) {
-    return getnextPosition(i, j + 1, 3)
-  }
-  if (endEdge == 4) {
-    return getnextPosition(i + 1, j, 4)
-  }
+    //check the end cell of the matrix::::::
+    if (i === matrix.length - 1 && j === matrix[0].length - 1) {
+      if (endEdge == 3) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
 
+    if (endEdge == 1) {
+      return getnextPosition(i, j - 1, 1)
+    }
+    if (endEdge == 2) {
+      return getnextPosition(i - 1, j, 2)
+    }
+    if (endEdge == 3) {
+      return getnextPosition(i, j + 1, 3)
+    }
+    if (endEdge == 4) {
+      return getnextPosition(i + 1, j, 4)
+    }
+  }
 }
 
 //connections between the cell of the matrix::::::
@@ -89,8 +92,19 @@ const connection = {
   4: 2
 }
 
+//console.time("executionTime");
+const startTime = performance.now();
+console.log("startTime::", startTime);
 //call for the function::::::
 console.log(getnextPosition(0, 0, 3));
+
+//console.timeEnd("executionTime");
+
+const endTime = performance.now();
+console.log("endTime::", endTime);
+// Calculate and display the execution time
+const executionTime = endTime - startTime;
+console.log("executionTime::", executionTime);
 
 //static for fix matrix::::::::::::
 // const matrix = [
@@ -143,4 +157,3 @@ console.log(getnextPosition(0, 0, 3));
 // } else {
 //   console.log(false);
 // }
-
